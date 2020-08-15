b4c4f44cc041a2fab01bd4788a37035efb296485
Michael Ballantyne <michael.ballantyne@gmail.com>
2020-08-15 10:29:34 -0600

    skip dot files in build script

--- code/build.sh
+++ code/build.sh
@@ -19,7 +19,7 @@ install dsls/type-expander
 
 raco setup --pkgs ee-lib cmdline-ee minikanren-ee racket-peg-ee linea shell-pipeline rash type-expander
 
-for f in $(find ./examples -name *.rkt)
+for f in $(find ./examples -not -path '*/\.*' -name *.rkt)
 do
   raco make $f
 done
